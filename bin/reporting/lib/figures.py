a5fca7f5e0b2e6376ea8f7ecfe9d717641e4deaf
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-02-02 14:22:38 +0000

    Added pdf export

--- bin/reporting/lib/figures.py
+++ bin/reporting/lib/figures.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import seaborn as sb
 
 class Figures:
@@ -54,6 +55,23 @@ class Figures:
         for key in data:
             data[key].to_csv(os.path.join(abs_path, key + '.csv'), index=False)
             plots[key].savefig(os.path.join(abs_path, key + '.png'))
+
+        # Save pdf of the plots
+        self.gen_pdf(abs_path, plots)
+
+    def gen_pdf(self, output_path, plots):
+        with PdfPages(os.path.join(output_path, 'report.pdf')) as pdf:
+            for key in plots:
+                pdf.savefig(plots[key])
+
+            # # We can also set the file's metadata via the PdfPages object:
+            # d = pdf.infodict()
+            # d['Title'] = 'Multipage PDF Example'
+            # d['Author'] = 'Jouni K. Sepp\xe4nen'
+            #        d['Subject'] = 'How to create a multipage pdf file and set its metadata'
+            # d['Keywords'] = 'PdfPages multipage keywords author title subject'
+            # d['CreationDate'] = datetime.datetime(2009, 11, 13)
+            # d['ModDate'] = datetime.datetime.today()
 
     ##### PLOTS #####
 
@@ -154,3 +172,45 @@ class Figures:
 # # plt.show()
 # fig2 = ax2.get_figure()
 # fig2.savefig('fragmanet_distribution_line.png')
+
+# import datetime
+# import numpy as np
+# from matplotlib.backends.backend_pdf import PdfPages
+# import matplotlib.pyplot as plt
+
+# # Create the PdfPages object to which we will save the pages:
+# # The with statement makes sure that the PdfPages object is closed properly at
+# # the end of the block, even if an Exception occurs.
+# with PdfPages('multipage_pdf.pdf') as pdf:
+#     plt.figure(figsize=(3, 3))
+#     plt.plot(range(7), [3, 1, 4, 1, 5, 9, 2], 'r-o')
+#     plt.title('Page One')
+#     pdf.savefig()  # saves the current figure into a pdf page
+#     plt.close()
+
+#     # if LaTeX is not installed or error caught, change to `usetex=False`
+#     plt.rc('text', usetex=True)
+#     plt.figure(figsize=(8, 6))
+#     x = np.arange(0, 5, 0.1)
+#     plt.plot(x, np.sin(x), 'b-')
+#     plt.title('Page Two')
+#     pdf.attach_note("plot of sin(x)")  # you can add a pdf note to
+#                                        # attach metadata to a page
+#     pdf.savefig()
+#     plt.close()
+
+#     plt.rc('text', usetex=False)
+#     fig = plt.figure(figsize=(4, 5))
+#     plt.plot(x, x ** 2, 'ko')
+#     plt.title('Page Three')
+#     pdf.savefig(fig)  # or you can pass a Figure object to pdf.savefig
+#     plt.close()
+
+#     # We can also set the file's metadata via the PdfPages object:
+#     d = pdf.infodict()
+#     d['Title'] = 'Multipage PDF Example'
+#     d['Author'] = 'Jouni K. Sepp\xe4nen'
+#     d['Subject'] = 'How to create a multipage pdf file and set its metadata'
+#     d['Keywords'] = 'PdfPages multipage keywords author title subject'
+#     d['CreationDate'] = datetime.datetime(2009, 11, 13)
+#     d['ModDate'] = datetime.datetime.today()
