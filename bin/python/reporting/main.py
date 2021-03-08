ae312c6db565905e55bf437da7b1bf52eae949a4
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-03-08 11:52:18 +0000

    Logging now working

--- bin/python/reporting/main.py
+++ bin/python/reporting/main.py
@@ -12,28 +12,36 @@ import logging
 
 from lib.figures import Figures
 
+def init_logger(app_name, log_file = None):
+    logger = logging.getLogger(app_name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s - %(message)s')
+
+    if log_file:
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    return logger
+
 def gen_png(parsed_args):
     input_path = parsed_args.input
     output_path = parsed_args.output
+    logger = init_logger('gen_img', parsed_args.log)
     
-    log_path = None
-    if parsed_args.log:
-        log_path = parsed_args.log
-        print(log_path)
-
-    logging.basicConfig(filename=log_path, level=os.environ.get("LOGLEVEL", "INFO"))
-    logger = logging.getLogger("gen_png")
     logger.info('Generating plots to output folder')
-
     fig = Figures(logger, input_path)
     fig.gen_plots_to_folder(output_path)
 
+    logger.info('Completed')
 
-if __name__ == '__main__':
-    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-    logger = logging.getLogger("app")
-    logger.info(sys.executable)
- 
+if __name__ == '__main__': 
     # Create command args
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='sub-command help')
@@ -47,4 +55,10 @@ if __name__ == '__main__':
 
     # Parse
     parsed_args = parser.parse_args()
+
+    # Init logging
+    logger = init_logger('reporting', parsed_args.log)
+    logger.info("Reporting app")
+
+    # Call functions
     parsed_args.func(parsed_args)
