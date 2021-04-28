f884095f49042e8a1fdb4549fa7b9d1b5df4e54c
Charlotte West <charlotte.west@oist.jp>
2021-04-28 09:25:10 +0100

    add bams to required input

--- bin/python/reporting/main.py
+++ bin/python/reporting/main.py
@@ -37,9 +37,10 @@ def gen_png(parsed_args):
     logger = init_logger('gen_img', parsed_args.log)
     bin_frag_path = parsed_args.bed_frag
     seacr_bed_path = parsed_args.seacr_bed
+    bams_path = parsed_args.bams
     
     logger.info('Generating plots to output folder')
-    fig = Figures(logger, meta_path, frag_path, bin_frag_path, seacr_bed_path)
+    fig = Figures(logger, meta_path, frag_path, bin_frag_path, seacr_bed_path, bams_path) 
     fig.gen_plots_to_folder(output_path)
 
     logger.info('Completed')
@@ -58,6 +59,7 @@ if __name__ == '__main__':
     parser_genimg.add_argument('--log', required=False)
     parser_genimg.add_argument('--bed_frag', required=True)
     parser_genimg.add_argument('--seacr_bed', required=True)
+    parser_genimg.add_argument('--bams', required=True)
 
     # Parse
     parsed_args = parser.parse_args()
