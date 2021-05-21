208e04683b7c6abec1499772aba9c85d89a3888c
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-05-21 09:10:43 +0100

    Updated schema file

--- lib/NfcoreSchema.groovy
+++ lib/NfcoreSchema.groovy
@@ -177,176 +177,8 @@ class NfcoreSchema {
         }
     }
 
-    // Loop over nested exceptions and print the causingException
-    private static void printExceptions(exJSON, paramsJSON, log) {
-        def causingExceptions = exJSON['causingExceptions']
-        if (causingExceptions.length() == 0) {
-            def m = exJSON['message'] =~ /required key \[([^\]]+)\] not found/
-            // Missing required param
-            if (m.matches()) {
-                log.error "* Missing required parameter: --${m[0][1]}"
-            }
-            // Other base-level error
-            else if (exJSON['pointerToViolation'] == '#') {
-                log.error "* ${exJSON['message']}"
-            }
-            // Error with specific param
-            else {
-                def param = exJSON['pointerToViolation'] - ~/^#\//
-                def param_val = paramsJSON[param].toString()
-                log.error "* --${param}: ${exJSON['message']} (${param_val})"
-            }
-        }
-        for (ex in causingExceptions) {
-            printExceptions(ex, paramsJSON, log)
-        }
-    }
-
-    // Remove an element from a JSONArray
-    private static JSONArray removeElement(jsonArray, element){
-        def list = []
-        int len = jsonArray.length()
-        for (int i=0;i<len;i++){
-            list.add(jsonArray.get(i).toString())
-        }
-        list.remove(element)
-        JSONArray jsArray = new JSONArray(list)
-        return jsArray
-    }
-
-    private static JSONObject removeIgnoredParams(rawSchema, params){
-        // Remove anything that's in params.schema_ignore_params
-        params.schema_ignore_params.split(',').each{ ignore_param ->
-            if(rawSchema.keySet().contains('definitions')){
-                rawSchema.definitions.each { definition ->
-                    for (key in definition.keySet()){
-                        if (definition[key].get("properties").keySet().contains(ignore_param)){
-                            // Remove the param to ignore
-                            definition[key].get("properties").remove(ignore_param)
-                            // If the param was required, change this
-                            if (definition[key].has("required")) {
-                                def cleaned_required = removeElement(definition[key].required, ignore_param)
-                                definition[key].put("required", cleaned_required)
-                            }
-                        }
-                    }
-                }
-            }
-            if(rawSchema.keySet().contains('properties') && rawSchema.get('properties').keySet().contains(ignore_param)) {
-                rawSchema.get("properties").remove(ignore_param)
-            }
-            if(rawSchema.keySet().contains('required') && rawSchema.required.contains(ignore_param)) {
-                def cleaned_required = removeElement(rawSchema.required, ignore_param)
-                rawSchema.put("required", cleaned_required)
-            }
-        }
-        return rawSchema
-    }
-
-    private static Map cleanParameters(params) {
-        def new_params = params.getClass().newInstance(params)
-        for (p in params) {
-            // remove anything evaluating to false
-            if (!p['value']) {
-                new_params.remove(p.key)
-            }
-            // Cast MemoryUnit to String
-            if (p['value'].getClass() == nextflow.util.MemoryUnit) {
-                new_params.replace(p.key, p['value'].toString())
-            }
-            // Cast Duration to String
-            if (p['value'].getClass() == nextflow.util.Duration) {
-                new_params.replace(p.key, p['value'].toString().replaceFirst(/d(?!\S)/, "day"))
-            }
-            // Cast LinkedHashMap to String
-            if (p['value'].getClass() == LinkedHashMap) {
-                new_params.replace(p.key, p['value'].toString())
-            }
-        }
-        return new_params
-    }
-
-     /*
-     * This method tries to read a JSON params file
-     */
-    private static LinkedHashMap params_load(String json_schema) {
-        def params_map = new LinkedHashMap()
-        try {
-            params_map = params_read(json_schema)
-        } catch (Exception e) {
-            println "Could not read parameters settings from JSON. $e"
-            params_map = new LinkedHashMap()
-        }
-        return params_map
-    }
-
-    private static Map log_colours(Boolean monochrome_logs) {
-        Map colorcodes = [:]
-
-        // Reset / Meta
-        colorcodes['reset']       = monochrome_logs ? '' : "\033[0m"
-        colorcodes['bold']        = monochrome_logs ? '' : "\033[1m"
-        colorcodes['dim']         = monochrome_logs ? '' : "\033[2m"
-        colorcodes['underlined']  = monochrome_logs ? '' : "\033[4m"
-        colorcodes['blink']       = monochrome_logs ? '' : "\033[5m"
-        colorcodes['reverse']     = monochrome_logs ? '' : "\033[7m"
-        colorcodes['hidden']      = monochrome_logs ? '' : "\033[8m"
-
-        // Regular Colors
-        colorcodes['black']       = monochrome_logs ? '' : "\033[0;30m"
-        colorcodes['red']         = monochrome_logs ? '' : "\033[0;31m"
-        colorcodes['green']       = monochrome_logs ? '' : "\033[0;32m"
-        colorcodes['yellow']      = monochrome_logs ? '' : "\033[0;33m"
-        colorcodes['blue']        = monochrome_logs ? '' : "\033[0;34m"
-        colorcodes['purple']      = monochrome_logs ? '' : "\033[0;35m"
-        colorcodes['cyan']        = monochrome_logs ? '' : "\033[0;36m"
-        colorcodes['white']       = monochrome_logs ? '' : "\033[0;37m"
-
-        // Bold
-        colorcodes['bblack']      = monochrome_logs ? '' : "\033[1;30m"
-        colorcodes['bred']        = monochrome_logs ? '' : "\033[1;31m"
-        colorcodes['bgreen']      = monochrome_logs ? '' : "\033[1;32m"
-        colorcodes['byellow']     = monochrome_logs ? '' : "\033[1;33m"
-        colorcodes['bblue']       = monochrome_logs ? '' : "\033[1;34m"
-        colorcodes['bpurple']     = monochrome_logs ? '' : "\033[1;35m"
-        colorcodes['bcyan']       = monochrome_logs ? '' : "\033[1;36m"
-        colorcodes['bwhite']      = monochrome_logs ? '' : "\033[1;37m"
-
-        // Underline
-        colorcodes['ublack']      = monochrome_logs ? '' : "\033[4;30m"
-        colorcodes['ured']        = monochrome_logs ? '' : "\033[4;31m"
-        colorcodes['ugreen']      = monochrome_logs ? '' : "\033[4;32m"
-        colorcodes['uyellow']     = monochrome_logs ? '' : "\033[4;33m"
-        colorcodes['ublue']       = monochrome_logs ? '' : "\033[4;34m"
-        colorcodes['upurple']     = monochrome_logs ? '' : "\033[4;35m"
-        colorcodes['ucyan']       = monochrome_logs ? '' : "\033[4;36m"
-        colorcodes['uwhite']      = monochrome_logs ? '' : "\033[4;37m"
-
-        // High Intensity
-        colorcodes['iblack']      = monochrome_logs ? '' : "\033[0;90m"
-        colorcodes['ired']        = monochrome_logs ? '' : "\033[0;91m"
-        colorcodes['igreen']      = monochrome_logs ? '' : "\033[0;92m"
-        colorcodes['iyellow']     = monochrome_logs ? '' : "\033[0;93m"
-        colorcodes['iblue']       = monochrome_logs ? '' : "\033[0;94m"
-        colorcodes['ipurple']     = monochrome_logs ? '' : "\033[0;95m"
-        colorcodes['icyan']       = monochrome_logs ? '' : "\033[0;96m"
-        colorcodes['iwhite']      = monochrome_logs ? '' : "\033[0;97m"
-
-        // Bold High Intensity
-        colorcodes['biblack']     = monochrome_logs ? '' : "\033[1;90m"
-        colorcodes['bired']       = monochrome_logs ? '' : "\033[1;91m"
-        colorcodes['bigreen']     = monochrome_logs ? '' : "\033[1;92m"
-        colorcodes['biyellow']    = monochrome_logs ? '' : "\033[1;93m"
-        colorcodes['biblue']      = monochrome_logs ? '' : "\033[1;94m"
-        colorcodes['bipurple']    = monochrome_logs ? '' : "\033[1;95m"
-        colorcodes['bicyan']      = monochrome_logs ? '' : "\033[1;96m"
-        colorcodes['biwhite']     = monochrome_logs ? '' : "\033[1;97m"
-
-        return colorcodes
-    }
-
     static String dashed_line(monochrome_logs) {
-        Map colors = log_colours(monochrome_logs)
+        Map colors = NfcoreTemplate.logColours(monochrome_logs)
         return "-${colors.dim}----------------------------------------------------${colors.reset}-"
     }
 
@@ -430,7 +262,7 @@ class NfcoreSchema {
      * Beautify parameters for --help
      */
     private static String params_help(workflow, params, json_schema, command) {
-        Map colors = log_colours(params.monochrome_logs)
+        Map colors = NfcoreTemplate.logColours(params.monochrome_logs)
         Integer num_hidden = 0
         String output  = ''
         output        += 'Typical pipeline command:\n\n'
@@ -556,7 +388,7 @@ class NfcoreSchema {
      * Beautify parameters for summary and return as string
      */
     private static String params_summary_log(workflow, params, json_schema) {
-        Map colors = log_colours(params.monochrome_logs)
+        Map colors = NfcoreTemplate.logColours(params.monochrome_logs)
         String output  = ''
         def params_map = paramsSummaryMap(workflow, params)
         def max_chars  = paramsMaxChars(params_map)
