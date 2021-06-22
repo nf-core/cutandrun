5b4b94015278f3d14433aefc9a6610c7dcb09d92
Charlotte West <charlotte.west@oist.jp>
2021-06-22 13:07:00 +0100

    change docker container name

--- dev/docker/consensus_peaks/run.sh
+++ dev/docker/consensus_peaks/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -v "$PWD":/home/repo -it luslab/cutandrun-dev-threshold-peaks:latest /home/repo/bin/consensus_peaks.py \
+docker run --rm -v "$PWD":/home/repo -it luslab/cutandrun-dev-plot-consensus-peaks:latest /home/repo/bin/consensus_peaks.py \
 --peaks /home/repo/dev/docker/consensus_peaks/test_data/*rep_thresh* \
 --outpath /home/repo/dev/docker/consensus_peaks/test_output
