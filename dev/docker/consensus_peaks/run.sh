5cf2c2e056c3b2f8256bc5e966d0d78a45636b2d
Charlotte West <charlotte.west@oist.jp>
2021-06-22 16:08:18 +0100

    update dev run.sh for consensus peaks

--- dev/docker/consensus_peaks/run.sh
+++ dev/docker/consensus_peaks/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run --rm -v "$PWD":/home/repo -it luslab/cutandrun-dev-plot-consensus-peaks:latest /home/repo/bin/consensus_peaks.py \
---peaks /home/repo/dev/docker/consensus_peaks/test_data/*rep_thresh* \
+--peaks /home/repo/dev/docker/consensus_peaks/test_data/*peaks.bed \
 --outpath /home/repo/dev/docker/consensus_peaks/test_output
