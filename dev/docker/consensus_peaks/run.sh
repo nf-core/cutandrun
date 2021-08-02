734abcd712fa7c98487bc40784145f220bea183a
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-08-02 14:57:34 +0100

    removed some dev solutions

--- dev/docker/consensus_peaks/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run --rm -v "$PWD":/home/repo -it luslab/cutandrun-dev-plot-consensus-peaks:latest /home/repo/bin/consensus_peaks.py \
---peaks /home/repo/dev/docker/consensus_peaks/test_data/*peaks.bed \
---outpath /home/repo/dev/docker/consensus_peaks/test_output
--- dev/docker/deseq2_diff/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile -t luslab/cutrun-ds2-dev:latest .
