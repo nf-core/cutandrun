eb08fa44053dc158b6f424c106d8fdcdb7371d3b
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-08-08 13:40:57 +0100

    updated scrape software versions

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -15,21 +15,20 @@ for version_file in version_files:
     results[software] = version
 
 # Dump to YAML
-print ('''
+print ("""
 id: 'software_versions'
-section_name: 'nf-core/rnaseq Software Versions'
-section_href: 'https://github.com/nf-core/rnaseq'
+section_name: 'nf-core/cutandrun Software Versions'
+section_href: 'https://github.com/nf-core/cutandrun'
 plot_type: 'html'
 description: 'are collected at run time from the software output.'
 data: |
     <dl class="dl-horizontal">
-"""
-)
-for k, v in sorted(results.items()):
+""")
+for k,v in sorted(results.items()):
     print("        <dt>{}</dt><dd><samp>{}</samp></dd>".format(k, v))
-print("    </dl>")
+print ("   </dl>")
 
 # Write out as tsv file:
 with open("software_versions.tsv", "w") as f:
-    for k, v in sorted(results.items()):
-        f.write("{}\t{}\n".format(k, v))
+    for k,v in sorted(results.items()):
+        f.write("{}\t{}\n".format(k,v))
