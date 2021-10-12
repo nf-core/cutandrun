72d934d10694617b4bd662fb5bbbe2c5e9136657
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-10-12 16:21:26 +0100

    Added peak repo code

--- bin/peak_reproducability.py
+++ bin/peak_reproducability.py
@@ -2,25 +2,24 @@
 
 import os
 import glob
-import re
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import argparse
-import upsetplot
+
+import deeptools.countReadsPerBin as crpb
+import pysam
 
 ############################################
 ############################################
 ## PARSE ARGUMENTS
 ############################################
 ############################################
-Description = 'Upset ven diagram of consensus peaks.'
-Epilog = """Example usage: python consensus_peaks.py <MERGED_INTERVAL_FILE> """
+Description = 'Calclate FRIP scores (FRagment proportion in Peaks regions) using deeptools for each sample'
 
-parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
+parser = argparse.ArgumentParser(description=Description)
 
 ## REQUIRED PARAMETERS
-parser.add_argument('--peaks', help="Merged peaks interval file with replicate counts column.")
+parser.add_argument('--bams', help="Bam file.")
+parser.add_argument('--peaks', help="Peaks interval file.")
+parser.add_argument('--threads', help="the number of threads for the task.")
 parser.add_argument('--outpath', help="Full path to output directory.")
 args = parser.parse_args()
 
@@ -30,45 +29,34 @@ args = parser.parse_args()
 ############################################
 ############################################
 
-# create list of data frames, one for each group consensus peaks file
+# https://deeptools.readthedocs.io/en/develop/content/example_api_tutorial.html
+
+# Create file lists
+bam_file_list = glob.glob(args.bams)
 peak_file_list = glob.glob(args.peaks)
-peak_df_list = list()
-for i in list(range(len(peak_file_list))):
-    peaks_i = pd.read_csv(peak_file_list[i], sep='\t', header=None, usecols=[0,1,2,8,9], names=['chrom','start','end','sample_reps','count'])
-    peaks_i['sample_reps'] = peaks_i['sample_reps'].replace(".peaks.bed.stringent.bed", "", regex=True)
-    peak_df_list.append(peaks_i)
-    reps2 = peaks_i[peaks_i["count"]>1]
 
-# add sorted column to each dataframe, and make new condensed dataframe list
-summary_peak_df_list = list()
-for i in list(range(len(peak_df_list))):
-    peaks_i = peak_df_list[i]
-    peaks_i['sorted_samples'] = ''
-    rows_now = peaks_i.shape[0]
-    for j in list(range(rows_now)):
-        sample_list = peaks_i.at[j,'sample_reps']
-        sample_array = np.unique(sample_list.split(','))
-        sample_sorted = sorted(sample_array)
-        sample_str = ",".join(sample_sorted)
-        peaks_i.at[j,'sorted_samples'] = sample_str
-    summary_peaks_i = peaks_i[['sorted_samples', 'count']].groupby(['sorted_samples'], as_index = False).sum()
-    summary_peak_df_list.append(summary_peaks_i)
+frips = []
+for idx, bam_file in enumerate(bam_file_list):
+    print("Calculating " + bam_file + " using " + peak_file_list[idx])
+    cr = crpb.CountReadsPerBin([bam_file], bedFile=[peak_file_list[idx]], numberOfProcessors=int(args.threads))
 
-# construct data in appropriate format for upsetplot, and plot
-for i in list(range(len(summary_peak_df_list))):
-    df_i = summary_peak_df_list[i]
-    # Get group name
-    basename = os.path.basename(peak_file_list[i])
-    group_name = basename.rsplit(".", -1)[0]
-    file_name = group_name + ".consensus_peaks.pdf"
-    categories = df_i.shape[0]
-    cat_list = []
-    for j in list(range(categories)):
-        summary_sample = df_i.at[j,'sorted_samples'].split(',')
-        cat_list.append(summary_sample)
+    # Calc the total number of reads in peaks per bam file
+    reads_at_peaks = cr.run()
+    total = reads_at_peaks.sum(axis=0)
 
-    # Plot
-    peak_counts = upsetplot.from_memberships(cat_list, data = df_i['count'])
-    upsetplot.plot(peak_counts)
-    plt.show()
-    plt.savefig(os.path.join(args.outpath, file_name))
+    # Load up bam file and get the total number of mapped reads
+    bam = pysam.AlignmentFile(bam_file)
+
+    # Calc frip
+    frip = float(total[0]) / bam.mapped
+    frips.append(str(frip))
+
+    # Log
+    print("Frip = " + str(frip))
+
+# Create string and write to file
+frip_string = ",".join(frips)
+writer = open(os.path.join(args.outpath, "frips.csv"), "w")
+writer.write("frip\n")
+writer.write(frip_string)
+writer.close()
---
+++ modules/local/modules/calculate_peak_reprod/main.nf
@@ -0,0 +1,32 @@
+include { initOptions; saveFiles; getSoftwareName } from './functions'
+
+params.options = [:]
+options        = initOptions(params.options)
+
+process CALCULATE_PEAK_REPROD {
+    label 'process_medium'
+    publishDir "${params.outdir}",
+        mode: params.publish_dir_mode,
+        saveAs: { filename -> saveFiles(filename:filename, options:params.options, publish_dir:getSoftwareName(task.process), publish_id:'') }
+
+    conda (params.enable_conda ? "conda-forge::python=3.8.3 bioconda::deeptools=3.5.* bioconda::pysam=0.17.*" : null)
+    container "luslab/cutandrun-dev-frip:latest"
+
+    input:
+    tuple val(meta), path(bam), path(bai), path(bed)
+
+    output:
+    tuple val(meta), path('peak_repro.csv'), emit: csv
+    path '*.version.txt'                   , emit: version
+
+    script:
+    """
+    frip.py \\
+        --bams "*.bam" \\
+        --peaks "*.bed" \\
+        --threads ${task.cpus} \\
+        --outpath .
+
+    python --version | grep -E -o \"([0-9]{1,}\\.)+[0-9]{1,}\" > python.version.txt
+    """
+}
