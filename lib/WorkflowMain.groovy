a0cf7eada83c2967546e47983f1057fac8e7f596
Chris Cheshire <chris.j.cheshire@gmail.com>
2021-11-08 13:39:12 +0000

    Added cite for all versions

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -9,7 +9,7 @@ class WorkflowMain {
     //
     public static String citation(workflow) {
         return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
-            "  https://doi.org/10.5281/zenodo.5653536\n\n" +
+            "  https://doi.org/10.5281/zenodo.5653535\n\n" +
             "* The nf-core framework\n" +
             "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
             "* Software dependencies\n" +
