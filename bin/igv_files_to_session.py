41594f411cf1d94e6bf3cd98aa5f1ce4145b81dc
Chris Cheshire <chris.j.cheshire@gmail.com>
2022-07-28 12:50:17 +0100

    Adjusted igv order

--- bin/igv_files_to_session.py
+++ bin/igv_files_to_session.py
@@ -104,11 +104,7 @@ def igv_files_to_session(XMLOut,ListFile,Genome,GtfBed,PathPrefix=''):
             XMLStr += '\t\t<Track altColor="0,0,178" autoScale="false" clazz="org.broad.igv.track.FeatureTrack" color="%s" ' % (colour)
             XMLStr += 'displayMode="SQUISHED" featureVisibilityWindow="-1" fontSize="12" height="20" '
             XMLStr += 'id="%s" name="%s" renderer="BASIC_FEATURE" sortable="false" visible="true" windowFunction="count"/>\n' % (ifile,os.path.basename(ifile))
-
-    ## Then the rest
-    for ifile,colour in fileList:
-        extension = os.path.splitext(ifile)[1].lower()
-        if extension in ['.bw', '.bigwig', '.tdf', '.bedGraph', '.bedgraph']:
+        elif extension in ['.bw', '.bigwig', '.tdf', '.bedGraph', '.bedgraph']:
             XMLStr += '\t\t<Track altColor="0,0,178" autoScale="true" clazz="org.broad.igv.track.DataSourceTrack" color="%s" ' % (colour)
             XMLStr += 'displayMode="COLLAPSED" featureVisibilityWindow="-1" fontSize="12" height="100" '
             XMLStr += 'id="%s" name="%s" normalize="false" renderer="BAR_CHART" sortable="true" visible="true" windowFunction="mean">\n' % (ifile,os.path.basename(ifile))
